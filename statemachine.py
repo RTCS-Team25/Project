c1bfbf7881c36bdd5f569065a433291b571f12af
leo-aero15-manjaro <LeoJhon.Song@outlook.com>
2020-11-04 21:48:28 +0800

    fix: add json file, fix encode bug and break bug, format code

--- statemachine.py
+++ statemachine.py
@@ -2,9 +2,9 @@ import time
 from naoqi import ALProxy
 import jstyleson
 
+
 def is_positive(word, positive, negative):
-       
-    # sorting responses                  
+    # sorting responses
     if word in positive:
         return 0                # return appropriate index for next state
     elif word in negative:
@@ -12,25 +12,26 @@ def is_positive(word, positive, negative):
     elif word == 'quit':
         return -1            # exit condition
 
+
 def fsm():
     '''
     conversation fsm
     '''
-    IP = "192.168.1.107"
+    IP = "pepper.local"  # use the alias for pepper's IP
     tts = ALProxy("ALTextToSpeech", IP, 9559)  # initiates text to speech functionality
     asr = ALProxy("ALSpeechRecognition", IP, 9559)  # initiates speech recognition functionality
     memory = ALProxy("ALMemory", IP, 9559)
 
     asr.setLanguage("English")
 
-    positive = ["good", "great", "yes", "okay", "hello", "hi"]     # can be expanded
-    negative = ["bad", "sad", "no"]   
+    positive = ["good", "great", "yes", "okay", "hello", "hi"]  # can be expanded
+    negative = ["bad", "sad", "no"]  # can be expanded
     vocab = positive + negative
 
     asr.pause(0)  # pause the ASR engine to be able to call `setVocabulary()`
     asr.setVocabulary(vocab, False)  # sets what pepper understands
     asr.pause(1)  # restart the ASR engine
-  
+
     with open('./script.json') as f:    # load script from the file
         script = jstyleson.load(f)
 
@@ -40,19 +41,21 @@ def fsm():
     asr.subscribe("nao")  # pepper start to listens, eyes turns blue
 
     while True:
-        tts.say(script[state]['content'])
+        tts.say(script[state]['content'].encode('ascii', 'ignore'))  # convert from unicode to ascii for compatibility
         time.sleep(5)           # delay to allow user time to reply
-        response = memory.getData("WordRecognized") # retrieve response
+        response = memory.getData("WordRecognized")  # retrieve response
+        print('response: ' + str(response) + '\n')
+        print('state: ' + state)
         feedback = is_positive(response[-2], positive, negative)    # sort response
-        if feedback == -1: #Detect if the user says 'quit'
-            break           #Break from loop
+        if feedback == -1 or state == '-1':  # Detect if the user says 'quit'
+            break  # Break from loop
         try:
             state = script[state]['next'][feedback]  # switch state of conversation
         except IndexError:
             state = script[state]['next'][0]    # for if only one state listed in "next"
 
-    tts.say('Goodbye!')
-    asr.unsubscribe("nao") # end listening
+    asr.unsubscribe("nao")  # end listening
+
 
 if __name__ == "__main__":
     fsm()
